3f73af361284ec40091e4fda5b00cd12c0f6e387
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-01-25 23:43:53 +0400

    Add project files.

---
+++ RPGSln/Controllers/CharacterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RPGSln.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+
+    }
+}
