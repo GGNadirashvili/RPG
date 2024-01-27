32adccba82fdbb7b6f6a8205e73497e311a854f7
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-01-27 15:20:55 +0400

    Adding Authentication Repository, Register User & Hash Password

---
+++ RPGSln/Data/AuthRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPGSln.Models;
+
+namespace RPGSln.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext context;
+
+        public AuthRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        public Task<ServiceResponse<string>> Login(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            var response = new ServiceResponse<int>();
+            if (await UserExists(user.UserName))
+            {
+                response.Succes = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswrodSalt = passwordSalt;
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // hash password
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
---
+++ RPGSln/Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using RPGSln.Models;
+
+namespace RPGSln.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(User user, string password);
+        Task<bool> UserExists(string username);
+    }
+}
