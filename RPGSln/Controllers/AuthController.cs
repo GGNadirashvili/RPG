86de3f10829955310215556ce24571c66e987f20
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-01-27 21:17:05 +0400

    Adding Authentication Controller

---
+++ RPGSln/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPGSln.Data;
+using RPGSln.Dtos.User;
+using RPGSln.Models;
+
+namespace RPGSln.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository authRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+            this.authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await authRepo.Register(
+                new User { UserName = request.Username }, request.Password
+                );
+            if (!response.Succes)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+
+}
