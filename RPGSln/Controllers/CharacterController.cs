9b37f555490719f4783977fc58748d706e50c8c3
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-01-28 13:15:27 +0400

    Adding Authentication Middleware & Authorize Attribute

--- RPGSln/Controllers/CharacterController.cs
+++ RPGSln/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RPGSln.Dtos.Character;
 using RPGSln.Models;
@@ -6,6 +7,7 @@ using RPGSln.Services.CharacterService;
 
 namespace RPGSln.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CharacterController : ControllerBase
